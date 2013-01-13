40e2f46ca5ff2944c3333f6f241d43431357b307
Andrey <munspel@rambler.ru>
2013-01-13 11:06:41 +0200

    121
    121

--- protected/runtime/gii-1.1.12/ModelCode.php
+++ protected/runtime/gii-1.1.12/ModelCode.php
@@ -3,7 +3,7 @@ return array (
   'template' => 'default',
   'connectionId' => 'db',
   'tablePrefix' => '',
-  'modelPath' => 'application.models',
+  'modelPath' => 'application.models.directory',
   'baseClass' => 'CActiveRecord',
   'buildRelations' => '0',
 );
