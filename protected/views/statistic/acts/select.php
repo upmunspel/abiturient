a4f8a92b8235bb2dc4bc528d7798081d4b28ad09
matmod <matmod@matmod-4>
2013-08-21 15:16:59 +0300

    + акты (статистика), но криво и говнокод

--- protected/views/statistic/acts/select.php
+++ protected/views/statistic/acts/select.php
@@ -1,11 +1,3 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html>
- <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <title>Формування рейтингу - крок 1</title>
-  <link rel="stylesheet" type="text/css" href="styles.css" />
- </head>
- <body>
 	<?php 
 	$fac = $_POST['facultet'];
 	$spec = $_POST['spec'];
@@ -21,15 +13,15 @@
         //echo $SpecSpec."\n";
         if($tmp == false){
         $speciality = substr($SpecSpec,0, $index);
-        $specialization = substr($SpecSpec,$index+1,strlen($SpecSpec)-($index+1));
+        $specialization = substr($SpecSpec,$index+2,strlen($SpecSpec)-($index+1));
         }
         else
         {
             $speciality = $SpecSpec;
             $specialization = "";
         }
-        echo $speciality;
-        echo "   ".$specialization;
+       // echo $speciality;
+       // echo "   ".$specialization;
 	$connection = mysql_connect('10.1.103.26','root','ehHYAuj');
 	mysql_query("use abiturient");
 	mysql_query("set names utf8");
@@ -37,7 +29,7 @@
 	mysql_query("UPDATE parametersquery SET value='".$spec."' WHERE code=17");
         mysql_query("UPDATE parametersquery SET value='".$specialization."' WHERE code=18");
         mysql_query("UPDATE parametersquery SET value='".$speciality."' WHERE code=19");
-	echo "<a href=\"http://10.1.11.57:8080/request_report-1.0/decanat.jsp?Speciality=$spec&Fac=$fac&iframe=true&width=1024&height=600\">Сформувати PDF</a>";
+        
+ header('Location: http://10.1.11.57:8080/request_report-1.0/decanat.jsp');
+	//echo "<a href=\"http://10.1.11.57:8080/request_report-1.0/decanat.jsp?Speciality=$spec&Fac=$fac&iframe=true&width=1024&height=600\">Сформувати PDF</a>";
 	?>
- </body>
-</html>
