a8bc0ca7f6d1f155d9c9e8fa79a82f3c13de24fa
matmod <matmod@matmod-4>
2013-08-23 09:45:59 +0300

    + stat

--- protected/modules/pfd/views/prices/stat.php
+++ protected/modules/pfd/views/prices/stat.php
@@ -64,37 +64,25 @@ $url='http://10.1.11.57:8080/request_report-1.0/price_sort_all_nomoney.jsp&ifram
                             'target'=>"_blank",
         ),
         'buttons'=>array(
-            array('label'=>'Денна та заочна', 'items'=>array(
-                array('label'=>'Бакалавр', 'url'=>Yii::app()->user->getPrintUrlstat(1)),
-                array('label'=>'Спеціаліст', 'url'=>Yii::app()->user->getPrintUrlstat(3)),
-                array('label'=>'Магістр', 'url'=>Yii::app()->user->getPrintUrlstat(2)),
-                //'---',
-               // array('label'=>'Separate link', 'url'=>'#'),
-            
-                )),
-        ),
-    )); ?>
-&nbsp;
-<?php $this->widget('bootstrap.widgets.TbButtonGroup', array(
-        //'type'=>'primary', // '', 'primary', 'info', 'success', 'warning', 'danger' or 'inverse'
-        'size'=>'large',
-        'htmlOptions'=>array(
-                            'target'=>"_blank",
-        ),
-        'buttons'=>array(
-            array('label'=>'Денна/Заочна', 'items'=>array(
-                array('label'=>'Денна','items'=>array(
+            array('label'=>'Звіт', 'items'=>array(
+                array('label'=>'Денна', 'items'=>array(
                     array('label'=>'Бакалавр', 'url'=>Yii::app()->user->getPrintUrlstatFORM(1,1)),
                     array('label'=>'Спеціаліст', 'url'=>Yii::app()->user->getPrintUrlstatFORM(1,3)),
                     array( 'label'=>'Магістр', 'url'=>Yii::app()->user->getPrintUrlstatFORM(1,2)),
                     array( 'label'=>'Усі рівні кваліфікації', 'url'=>Yii::app()->user->getPrintUrlstatFORM(1,0))),
                     ),
                 array('label'=>'Заочна','items'=>array(
-                    array('label'=>'Бакалавр', 'url'=>Yii::app()->user->getPrintUrlstatFORM(2,1)),
-                    array('label'=>'Спеціаліст', 'url'=>Yii::app()->user->getPrintUrlstatFORM(2,3)),
+                    array('label'=>'Бакалавр', 'url'=>Yii::app()->user->getPrintUrlstatFORM(1,2)),
+                    array('label'=>'Спеціаліст', 'url'=>Yii::app()->user->getPrintUrlstatFORM(3,2)),
                     array( 'label'=>'Магістр', 'url'=>Yii::app()->user->getPrintUrlstatFORM(2,2)),
-                    array( 'label'=>'Усі рівні кваліфікації', 'url'=>Yii::app()->user->getPrintUrlstatFORM(2,0))),
+                    array( 'label'=>'Усі рівні кваліфікації', 'url'=>Yii::app()->user->getPrintUrlstatFORM(0,2))),
                     ),
+                array('label'=>'Денна та заочна','items'=>array(
+                    array('label'=>'Бакалавр', 'url'=>Yii::app()->user->getPrintUrlstat(1)),
+                    array('label'=>'Спеціаліст', 'url'=>Yii::app()->user->getPrintUrlstat(3)),
+                    array('label'=>'Магістр', 'url'=>Yii::app()->user->getPrintUrlstat(2)),
+                    array( 'label'=>'Усі рівні кваліфікації', 'url'=>Yii::app()->user->getPrintUrlstat(0)),         
+                )),
                 )),
         ),
     )); ?>
