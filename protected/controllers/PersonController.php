922c87715f69a36d9e6056ddb062452999af3e2e
matmod <matmod@matmod-5>
2014-07-17 11:04:46 +0300

    Исправлина валидация копий/оригиналов документов в заявке

--- protected/controllers/PersonController.php
+++ protected/controllers/PersonController.php
@@ -51,10 +51,10 @@ class PersonController extends Controller {
      */
     public function accessRules() {
         return array(
-            array('allow', // allow all users to perform 'index' and 'view' actions
+            /*array('allow', // allow all users to perform 'index' and 'view' actions
                 'actions' => array('index', 'view'),
                 'users' => array('*'),
-            ),
+            ),*/
             /* array('allow', // allow authenticated user to perform 'create' and 'update' actions
               'actions'=>array('create','update'),
               'users'=>array('@'),
