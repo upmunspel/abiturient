6c868f0117d2612a64087afe4560d97b13a3c456
IT.ZNU.EDU <sinepolskiyihor@gmail.com>
2014-07-17 18:27:13 +0300

    PFD SpecLicense, autoWordPrice, fix SpecAdmin

--- protected/models/directory/Specialities.php
+++ protected/models/directory/Specialities.php
@@ -6,6 +6,7 @@
  * The followings are the available columns in table 'specialities':
  * @property integer $idSpeciality
  * @property string $SpecialityName
+ * @property string $SpecialityLicenseName
  * @property string $SpecialityKode
  * @property integer $FacultetID
  * @property string $SpecialityClasifierCode
@@ -180,6 +181,7 @@ class Specialities extends CActiveRecord {
             array('idSpeciality, FacultetID, SpecialityBudgetCount, 
                               SpecialityContractCount, isZaoch, isPublishIn', 'numerical', 'integerOnly' => true),
             array('SpecialityName', 'length', 'max' => 100),
+            array('SpecialityLicenseName', 'length', 'max' => 300),
             array('SpecialityKode', 'length', 'max' => 40),
             array('SpecialityClasifierCode', 'length', 'max' => 12),
             array("WordPrice, StudyPeriodID, basespecialitys", "safe"),
@@ -189,7 +191,7 @@ class Specialities extends CActiveRecord {
             // Please remove those attributes that should not be searched.
             array('idSpeciality, SpecialityName, SpecialityKode, 
                             FacultetID, SpecialityClasifierCode, SpecialityBudgetCount, SpecialityContractCount, isZaoch, isPublishIn, Quota1, Quota2,
-                            WordPrice, YearPrice', 'safe', 'on' => 'search'),
+                            WordPrice, YearPrice, SpecialityLicenseName', 'safe', 'on' => 'search'),
         );
     }
 
@@ -213,6 +215,7 @@ class Specialities extends CActiveRecord {
         return array(
             'idSpeciality' => 'Id Speciality',
             'SpecialityName' => 'Спеціальність',
+            'SpecialityLicenseName' => 'Спеціальність згідно ліцензії',
             'SpecialityKode' => 'Speciality Kode',
             'FacultetID' => 'Facultet',
             'SpecialityClasifierCode' => 'Speciality Clasifier Code',
@@ -288,6 +291,7 @@ class Specialities extends CActiveRecord {
 
         $criteria->compare('idSpeciality', $this->idSpeciality);
         $criteria->compare('SpecialityName', $this->SpecialityName, true);
+        $criteria->compare('SpecialityLicenseName', $this->SpecialityLicenseName, true);
         $criteria->compare('SpecialityKode', $this->SpecialityKode, true);
         $criteria->compare('FacultetID', $this->FacultetID);
         $criteria->compare('SpecialityClasifierCode', $this->SpecialityClasifierCode, true);
@@ -303,7 +307,7 @@ class Specialities extends CActiveRecord {
         return new CActiveDataProvider($this, array(
             'criteria' => $criteria,
             'pagination' => array(
-                'pageSize' => 10000,
+                'pageSize' => 50,
             ),
             'sort' => array(
                 'attributes' => array("",
@@ -366,5 +370,62 @@ class Specialities extends CActiveRecord {
         }
         return $res;
     }
+    
+  /**
+   * Повертає суму прописом. Видрав із http://habrahabr.ru/post/53210/ .
+   * @author runcore
+   * @uses morph(...)
+   */
+  public function num2str($num) {
+      $nul='нуль';
+      $ten=array(
+          array('','один','два','три','чотири','п\'ять','шість','сім', 'вісім','дев\'ять'),
+          array('','одна','дві','три','чотири','п\'ять','шість','сім', 'вісімь','дев\'ять'),
+      );
+      $a20=array('десять','одинадцять','дванадцять','тринадцять','чотирнадцять' ,'п\'ятнадцять','шістнадцять','сімнадцять','вісімнадцять','дев\'ятнадцять');
+      $tens=array(2=>'двадцять','тридцять','сорок','п\'ятдесят','шістдесят','сімдесят' ,'вісімдесят','дев\'яносто');
+      $hundred=array('','сто','двісті','триста','чотириста','п\'ятсот','шістсот', 'сімсот','вісімсот','дев\'ятсот');
+      $unit=array( // Units
+          array('коп.' ,'коп.' ,'коп.',   0),
+          array('гривня'   ,'гривні'   ,'гривень' ,1),
+          array('тисяча'  ,'тисячи'  ,'тисяч'     ,1),
+          array('мільйон' ,'мільйона','мільйонів' ,0),
+          array('мільярд','мільярда','мільярдів'  ,0),
+      );
+      //
+      list($uah,$kop) = explode('.',sprintf("%015.2f", floatval($num)));
+      $out = array();
+      if (intval($uah)>0) {
+          foreach(str_split($uah,3) as $uk=>$v) { // by 3 symbols
+              if (!intval($v)) continue;
+              $uk = sizeof($unit)-$uk-1; // unit key
+              $gender = $unit[$uk][3];
+              list($i1,$i2,$i3) = array_map('intval',str_split($v,1));
+              // mega-logic
+              $out[] = $hundred[$i1]; # 1xx-9xx
+              if ($i2>1) $out[]= $tens[$i2].' '.$ten[$gender][$i3]; # 20-99
+              else $out[]= $i2>0 ? $a20[$i3] : $ten[$gender][$i3]; # 10-19 | 1-9
+              // units without uah & kop
+              if ($uk>1) $out[]= $this->morph($v,$unit[$uk][0],$unit[$uk][1],$unit[$uk][2]);
+          } //foreach
+      }
+      else $out[] = $nul;
+      $out[] = $this->morph(intval($uah), $unit[1][0],$unit[1][1],$unit[1][2]); // uah
+      $out[] = $kop.' '.$this->morph($kop,$unit[0][0],$unit[0][1],$unit[0][2]); // kop
+      return trim(preg_replace('/ {2,}/', ' ', join(' ',$out)));
+  }
+
+  /**
+   * Відмінює форму
+   * @ author runcore
+   */
+  protected function morph($n, $f1, $f2, $f5) {
+      $n = abs(intval($n)) % 100;
+      if ($n>10 && $n<20) return $f5;
+      $n = $n % 10;
+      if ($n>1 && $n<5) return $f2;
+      if ($n==1) return $f1;
+      return $f5;
+  }
 
 }
