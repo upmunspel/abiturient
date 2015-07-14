1a20594b52f33081e50eac1ae389bad44e8e7463
dobro <dobro@gen.znu.edu.ua>
2015-07-14 16:17:10 +0300

    + Звіт "Перелік осіб із ознакою "Перехресний вступ"

--- protected/modules/statistic/views/statistic/statgraduated.php
+++ protected/modules/statistic/views/statistic/statgraduated.php
@@ -61,13 +61,13 @@ H1 {
   <th colspan="6" align='center' style="font-size:10pt;">Заочна<br/>кількість</th>
   </tr>
   <tr>
-  <th align='center' style="font-size:10pt;">Вип. 2014</th>
+  <th align='center' style="font-size:10pt;">Вип. 2015</th>
   <th align='center' style="font-size:10pt;">ЗНУ</th>
   <th align='center' style="font-size:10pt;">Інші</th>
   <th align='center' style="font-size:10pt;">Всього</th>
   <th align='center' style="font-size:10pt;">Бюдж. місця</th>
   <th align='center' style="font-size:10pt;">Оригінали</th>
-  <th align='center' style="font-size:10pt;">Вип. 2014</th>
+  <th align='center' style="font-size:10pt;">Вип. 2015</th>
   <th align='center' style="font-size:10pt;">ЗНУ</th>
   <th align='center' style="font-size:10pt;">Інші</th>
   <th align='center' style="font-size:10pt;">Всього</th>
@@ -87,14 +87,10 @@ foreach ($cnt_data as $row){
     }
     echo "<td style='font-size: 10pt;'>".$key."</td>";
     
-    echo "<td style='color: grey;'>".((isset($data[1]))? 
-            $data[1]['graduated'] : '0') ."</td>";
-    echo "<td>".((isset($data[1]))? 
-            $data[1]['cnt_requests_from_us'] : '0') ."</td>";
-    echo "<td>".((isset($data[1]))? 
-            (($data[1]['cnt_requests_from_aliens'])? "<a href='".Yii::app()->request->baseUrl."/statistic/rept/index?fields=0%2C7%2C13%2C12%2C18%2C24&cond%5B0%5D=0&cond%5B1%5D=0&acondval%5B1%5D=&cond%5B2%5D=0&acondval%5B2%5D=&cond%5B3%5D=0&cond%5B4%5D=0&cond%5B5%5D=0&cond%5B6%5D=0&cond%5B7%5D=1&condval%5B7%5D=&acondval%5B7%5D=".$data[1]['idSpec']."&cond%5B8%5D=0&cond%5B9%5D=0&cond%5B10%5D=0&cond%5B11%5D=0&cond%5B12%5D=0&cond%5B13%5D=5&condval%5B13%5D=".$DateFrom."&acondval%5B13%5D=".$DateTo."&cond%5B14%5D=0&cond%5B15%5D=0&cond%5B16%5D=0&cond%5B17%5D=0&cond%5B18%5D=3&condval%5B18%5D=%D0%97%D0%B0%D0%BF%D0%BE%D1%80%D1%96%D0%B7%D1%8C%D0%BA%D0%B8%D0%B9+%D0%BD%D0%B0%D1%86%D1%96%D0%BE%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D0%B8%D0%B9+%D1%83%D0%BD%D1%96%D0%B2%D0%B5%D1%80%D1%81%D0%B8%D1%82%D0%B5%D1%82&cond%5B19%5D=0&cond%5B20%5D=0&cond%5B21%5D=0&cond%5B22%5D=0&cond%5B23%5D=0&cond%5B24%5D=2&condval%5B24%5D=%D0%94%D0%B8%D0%BF%D0%BB%D0%BE%D0%BC&yt2='>".$data[1]['cnt_requests_from_aliens'] . '</a>' : '0') : '0') ."</td>";
-    echo "<td>".((isset($data[1]))? 
-            ($data[1]['cnt_requests_from_us']+$data[1]['cnt_requests_from_aliens']) : '0') ."</td>";
+    echo "<td style='color: grey;'>".((isset($data[1]))? $data[1]['graduated'] : '0') ."</td>";
+    echo "<td>".((isset($data[1]))? $data[1]['cnt_requests_from_us'] : '0') ."</td>";
+    echo "<td>".((isset($data[1]))? (($data[1]['cnt_requests_from_aliens'])? "<a href='".Yii::app()->request->baseUrl."/statistic/rept/index?fields=0%2C7%2C13%2C12%2C18%2C24&cond%5B0%5D=0&cond%5B1%5D=0&acondval%5B1%5D=&cond%5B2%5D=0&acondval%5B2%5D=&cond%5B3%5D=0&cond%5B4%5D=0&cond%5B5%5D=0&cond%5B6%5D=0&cond%5B7%5D=1&condval%5B7%5D=&acondval%5B7%5D=".$data[1]['idSpec']."&cond%5B8%5D=0&cond%5B9%5D=0&cond%5B10%5D=0&cond%5B11%5D=0&cond%5B12%5D=0&cond%5B13%5D=5&condval%5B13%5D=".$DateFrom."&acondval%5B13%5D=".$DateTo."&cond%5B14%5D=0&cond%5B15%5D=0&cond%5B16%5D=0&cond%5B17%5D=0&cond%5B18%5D=3&condval%5B18%5D=%D0%97%D0%B0%D0%BF%D0%BE%D1%80%D1%96%D0%B7%D1%8C%D0%BA%D0%B8%D0%B9+%D0%BD%D0%B0%D1%86%D1%96%D0%BE%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D0%B8%D0%B9+%D1%83%D0%BD%D1%96%D0%B2%D0%B5%D1%80%D1%81%D0%B8%D1%82%D0%B5%D1%82&cond%5B19%5D=0&cond%5B20%5D=0&cond%5B21%5D=0&cond%5B22%5D=0&cond%5B23%5D=0&cond%5B24%5D=2&condval%5B24%5D=%D0%94%D0%B8%D0%BF%D0%BB%D0%BE%D0%BC&yt2='>".$data[1]['cnt_requests_from_aliens'] . '</a>' : '0') : '0') ."</td>";
+    echo "<td>".((isset($data[1]))? ($data[1]['cnt_requests_from_us']+$data[1]['cnt_requests_from_aliens']) : '0') ."</td>";
     echo "<td style='font-style: italic;'>".((isset($data[1]))? 
             0+$data[1]['cnt_req_budget'] : '0') ."</td>";
     echo "<td style='border-right: 5px solid grey;'>".((isset($data[1]))? 
