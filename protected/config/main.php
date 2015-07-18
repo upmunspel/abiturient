c97bd7a5dd528ba469600abd1d11a39eab558530
munspel <munspel@Matmod-5.znu.edu.ua>
2015-07-18 12:19:09 +0300

    Merge origin/master
    Conflicts:
    	protected/config/main.php

--- protected/config/main.php
+++ protected/config/main.php
@@ -135,7 +135,8 @@ return array(
           'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
           ), */
         // uncomment the following to use a MySQL database
-        'db' => require(__DIR__ . '/dp.php'),
+        'db' => require(__DIR__ . '/db.php'),
+      
         'errorHandler' => array(
             // use 'site/error' action to display errors
             'errorAction' => 'site/error',
